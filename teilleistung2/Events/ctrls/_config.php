05077ed725b74a7ba2ecf0d6518fdcccba055c0f
Markus <MKaettner@gmx.de>
2013-06-11 11:09:26 +0200

    Config angepasst

--- teilleistung2/Events/ctrls/_config.php
+++ teilleistung2/Events/ctrls/_config.php
@@ -1,4 +1,4 @@
 <?php
 define('HOST', 'localhost'); // Host auf dem die DB läuft. Localhost sollte i.d.R. passen
-define('USER', 'hier_den_benutzernamen'); // hier den Nutzer der Datenbank
-define('PASS', 'hier_das_passwort'); // hier das Passwort des Nutzers
+define('USER', 'root'); // hier den Nutzer der Datenbank
+define('PASS', ''); // hier das Passwort des Nutzers
