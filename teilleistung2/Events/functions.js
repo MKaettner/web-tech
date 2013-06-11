b6641a658933f04c6c66cf111eb1c5a4536a50bd
Markus <MKaettner@gmx.de>
2013-06-11 16:34:23 +0200

    Keine Ahnung...

--- teilleistung2/Events/functions.js
+++ teilleistung2/Events/functions.js
@@ -1,4 +1,9 @@
 function register(){
+	var name = document.getElementsByName('name').value;
+	var pw = document.getElementsByName('password').value;
+	//window.alert(name);
+	window.alert(pw);
+	
 	$.getJSON('ctrls/users.php', {
         operation: 'register',
         name: 'Nutzername',
@@ -6,6 +11,6 @@ function register(){
     },
 	function(data) {
 		// hier kann die Antwort des Servers verarbeitet werden!
-		window.alert(data.msg);
 	} );
+	
 }
