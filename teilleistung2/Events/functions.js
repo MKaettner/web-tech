8d1323e614552f2122446c64c8db4bee1d12433b
Markus <MKaettner@gmx.de>
2013-06-11 16:18:41 +0200

    dsf

--- teilleistung2/Events/functions.js
+++ teilleistung2/Events/functions.js
@@ -1,8 +1,4 @@
 function register(){
-	/*
-	var name;
-	var pw;
-	
 	$.getJSON('ctrls/users.php', {
         operation: 'register',
         name: 'Nutzername',
@@ -12,5 +8,4 @@ function register(){
 		// hier kann die Antwort des Servers verarbeitet werden!
 		window.alert(data.msg);
 	} );
-	*/ window.alert("Diese Funktion soll irgendwann registrieren");
 } 
