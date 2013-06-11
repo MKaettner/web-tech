6f8575777f330633b1f7521a4f104a4a7fa296e5
Markus <MKaettner@gmx.de>
2013-06-11 10:52:18 +0200

    Startup

---
+++ teilleistung2/Events/ctrls/_config.php
@@ -0,0 +1,4 @@
+<?php
+define('HOST', 'localhost'); // Host auf dem die DB läuft. Localhost sollte i.d.R. passen
+define('USER', 'hier_den_benutzernamen'); // hier den Nutzer der Datenbank
+define('PASS', 'hier_das_passwort'); // hier das Passwort des Nutzers
