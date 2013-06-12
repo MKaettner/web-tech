1d84f121d967855beb4e65cb2bb0f1f210d88270
Markus <MKaettner@gmx.de>
2013-06-12 12:43:39 +0200

    Methode funktioniert….. irgendwie

--- teilleistung2/Events/functions.js
+++ teilleistung2/Events/functions.js
@@ -1,15 +1,16 @@
 function register(){
 	
-	var name = document.getElementById("username").value;
-	var pwd = document.getElementById("userpwd").value;
+	var username = document.getElementById("username").value;
+	var userpassword = document.getElementById("userpwd").value;
 	
-	$.getJSON('ctrls/users.php', {
-        operation: 'register',
-        name: 'Nutzername',
-        password: 'Passwort'
+	$.getJSON("ctrls/users.php", {
+        operation: "register",
+        name: username,
+        password: userpassword
     },
 	function(data) {
 		// hier kann die Antwort des Servers verarbeitet werden!
+		alert(JSON.stringify(data));
 	} ); 
 	
 }
