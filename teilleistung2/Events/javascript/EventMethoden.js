cf0880ebb51ae2da349f70ab4e01bb40c7985f63
Markus <MKaettner@gmx.de>
2013-06-14 10:15:51 +0200

    list-events

--- teilleistung2/Events/javascript/EventMethoden.js
+++ teilleistung2/Events/javascript/EventMethoden.js
@@ -1,3 +1,25 @@
 function list_events(){
-	alert("aufgerufen");
+	$.getJSON('ctrls/events.php', {
+        operation: 'list_events'
+    },
+	function(data) {
+	//leert die Tabelle
+		$("#datatable").html("");
+		//fuegt den tableheader ein
+		$("#datatable").append(
+				"<tr> <th>Event ID</th> <th>Titel</th> <th>Beschreibung</th> <th>Datum</th> </tr>"
+		);
+		
+		$.each(data.events, function(key, value){
+			// auslesen der JSON Objekte in Variablen
+			var id = value.eid;
+			var title = value.title;
+			var desc = value.description;
+			var date = value.date;
+			// tabellenzeile hinzufügen
+			$("#datatable").append(
+				"<tr> <td> " + id + "</td> <td> " + title + "</td> <td> " + desc + "</td> <td> " + date + "</td>"
+			);
+		});
+	});
 }
