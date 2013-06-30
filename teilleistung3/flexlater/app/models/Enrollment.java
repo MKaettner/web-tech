4a381c13839cf70be9004d0a315051dee28ec93f
JaschaK <jascha_karp@web.de>
2013-06-30 13:56:00 +0200

    ...

--- teilleistung3/flexlater/app/models/Enrollment.java
+++ teilleistung3/flexlater/app/models/Enrollment.java
@@ -10,25 +10,28 @@ import javax.persistence.*;
 @Entity
 public class Enrollment extends Model {
 
+	// Attribute
 	@Id
 	public int enrollmentId;
+
 	/**
 	 * merged from testId and studentId
 	 */
 
-	// public static Finder<Long, Enrollment> find = new Finder(Long.class,
-	// Enrollment.class);
-	//
-	// public static List<Enrollment> all() {
-	// return Enrollment.all();
-	// }
-	//
-	// public static void create(Enrollment enrollment) {
-	// enrollment.save();
-	// }
-	// public static void delete(int enrollmentId) {
-	//
-	// }
-	//
+	// Methoden
+
+	public static Enrollment find(int enrollmentId) {
+		return null;
+	}
+
+	public static List<Enrollment> all() {
+		return null;
+	}
+
+	public static void create(Enrollment enrollment) {
+	}
+
+	public static void delete(int enrollmentId) {
+	}
 
 }
--- teilleistung3/flexlater/app/models/Exam.java
+++ teilleistung3/flexlater/app/models/Exam.java
@@ -1,5 +1,33 @@
 package models;
 
-public class Exam {
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.db.ebean.Model;
+
+@Entity
+public class Exam extends Model {
+
+	// Attribute
+	@Id
+	public int examId;
+
+	// Methoden
+
+	public static Exam find(int examId) {
+		return null;
+	}
+
+	public static List<Exam> all() {
+		return null;
+	}
+
+	public static void create(Exam exam) {
+	}
+
+	public static void delete(int examId) {
+	}
 
 }
