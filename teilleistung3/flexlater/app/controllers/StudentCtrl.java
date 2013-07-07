399fdd52218fd9e42ea6f2a8890008206d3b4ba2
JaschaK <jascha_karp@web.de>
2013-07-07 13:53:43 +0200

    Docent

--- teilleistung3/flexlater/app/controllers/StudentCtrl.java
+++ teilleistung3/flexlater/app/controllers/StudentCtrl.java
@@ -8,24 +8,24 @@ public class StudentCtrl extends Controller {
 
 	static Form<Student> studentForm = Form.form(Student.class);
 
-	
 	public static Result students() {
 		return ok(views.html.students.render(Student.all(), studentForm));
 	}
-	
+
 	public static Result newStudent() {
 		Form<Student> filledForm = studentForm.bindFromRequest();
 		if (filledForm.hasErrors()) {
-			return badRequest(views.html.students.render(Student.all(), filledForm));
+			return badRequest(views.html.students.render(Student.all(),
+					filledForm));
 		} else {
 			Student.create(filledForm.get());
 			return redirect(routes.StudentCtrl.students());
 		}
 	}
-	
+
 	public static Result deleteStudent(Long studentId) {
 		Student.delete(studentId);
 		return redirect(routes.StudentCtrl.students());
 	}
-	
+
 }
--- teilleistung3/flexlater/app/models/Docent.java
+++ teilleistung3/flexlater/app/models/Docent.java
@@ -10,29 +10,26 @@ import play.db.ebean.Model;
 @Entity
 public class Docent extends Model {
 
-		// Attribute
-		@Id
-		public Long docentId;
-		public String name, firstName, chair;
-		
+	// Attribute
+	@Id
+	public Long docentId;
+	public String name, firstName, chair;
 
-		// Methoden
+	// Methoden
 
-		public static Finder<Long, Docent> find = new Finder (Long.class, Docent.class);
+	public static Finder<Long, Docent> find = new Finder(Long.class,
+			Docent.class);
 
-		public static List<Docent> all() {
-			return find.all();
-		}
-
-		public static void create(Docent docent) {
-			docent.save();
-		}
-
-		public static void delete(Long docentId) {
-			find.ref(docentId).delete();
-		}
-
-		
+	public static List<Docent> all() {
+		return find.all();
 	}
-	
+
+	public static void create(Docent docent) {
+		docent.save();
+	}
+
+	public static void delete(Long docentId) {
+		find.ref(docentId).delete();
+	}
+
 }
