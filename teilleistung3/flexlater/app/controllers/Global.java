7c607c096030a305453a99108de858cbd9004b74
JaschaK <jascha_karp@web.de>
2013-07-10 21:54:00 +0200

    View & YAML

---
+++ teilleistung3/flexlater/app/controllers/Global.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Map;
+
+import models.*;
+import play.Application;
+import play.GlobalSettings;
+import play.libs.Yaml;
+
+import com.avaje.ebean.Ebean; 
+ 
+public class Global extends GlobalSettings {
+ 
+  @Override
+  public void onStart(Application app) {
+    if(Ebean.find(Student.class).findRowCount() == 0) {
+      Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
+      Ebean.save(all.get("students"));
+    }
+    if(Ebean.find(Docent.class).findRowCount() == 0) {
+        Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
+        Ebean.save(all.get("docents"));
+      }
+  }
+}
---
+++ teilleistung3/flexlater/public/javascripts/main.js
@@ -0,0 +1,126 @@
+$(function() { 
+    $('#examName').popover({ 
+        title: 'Format Bezeichnung',
+        content: 'Die Bezeichnung für die Prüfung, z.B. <i>Web-Technologien</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examPlace').popover({ 
+        title: 'Format Ort',
+        content: 'Ort und Raum für die Prüfung, z.B. <i>WE5/00.017</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examDate').popover({ 
+        title: 'Format Datum',
+        content: 'Das Datum für die Prüfung im Format dd.mm.yyyy, z.B. <i>15.08.2013</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examTime').popover({ 
+        title: 'Format Uhrzeit',
+        content: 'Die Uhrzeit zu der die Prüfung beginnt im Format hh:mm, z.B. <i>09:30</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examDocent').popover({ 
+        title: 'Format Dozent',
+        content: 'Der Name des Dozenten der die Prüfung stellt, z.B. <i>Prof. Henrich</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examMajor').popover({ 
+        title: 'Format Studiengang',
+        content: 'Der Studiengang dessen Studenten sich für diese Prüfung anmelden können, z.B. <i>Wirtschaftsinformatik</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examDuration').popover({ 
+        title: 'Format Dauer',
+        content: 'Die Dauer dieser Prüfung in Minuten, z.B. <i>90</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#examParticipant').popover({ 
+        title: 'Format Maximale Teilnehmerzahl',
+        content: 'Die maximale Zahl an Prüfungsteilnehmern, z.B. <i>50</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#studentName').popover({ 
+        title: 'Format Nachname',
+        content: 'Der Nachname des Studierenden, z.B. <i>Musterfrau</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#studentFirstName').popover({ 
+        title: 'Format Vorname',
+        content: 'Der Vorname des Studierenden, z.B. <i>Erika</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#studentMajor').popover({ 
+        title: 'Format Studiengang',
+        content: 'Der Studiengang des Studierenden, z.B. <i>Wirtschaftsinformatik</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#docentName').popover({ 
+        title: 'Format Nachname',
+        content: 'Der Nachname des Dozierenden, z.B. <i>Mustermann</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#docentFirstName').popover({ 
+        title: 'Format Vorname',
+        content: 'Der Vorname des Dozierenden, z.B. <i>Max</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
+$(function() { 
+    $('#docentChair').popover({ 
+        title: 'Format Lehrstuhl',
+        content: 'Die Bezeichnung des Lehrstuhls, z.B. <i>Medieninformatik</i>',
+        placement: 'top',
+        html: true,
+        trigger: 'hover'
+    });
+});
